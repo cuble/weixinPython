cd46215b87f9853211f9731f739b7537ffeb3e46
cuble[VMware Linux] <fouryu@sina.com.cn>
2014-01-06 23:13:51 +0800

    confirm yunCheng interface split out successful

--- weixinServer.py
+++ weixinServer.py
@@ -1,30 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-#import posixpath
-#import urllib
-#import shutil
-#import os
-#from os  import path
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-#import socket
 from SocketServer import ThreadingMixIn
 import threading
-#import sys
 import hashlib
-import timeHelper
 import cgi
-import xml.etree.ElementTree as ET
 import yunCheng4Weixin
 
-RESPONSE_TEXT_TEMPLATE = '''
-<xml>
-<ToUserName><![CDATA[{TO_USER}]]></ToUserName>
-<FromUserName><![CDATA[{FROM_USER}]]></FromUserName>
-<CreateTime>{TIME_STEMP}</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[{RESPONSE_CONTENT}]]></Content>
-</xml>
-'''  
 
 
 class Handler( BaseHTTPRequestHandler ):
@@ -57,40 +39,9 @@ class Handler( BaseHTTPRequestHandler ):
 		self.send_response(200)
 		self.end_headers()
 
-		dataDict = self.xmlToDict(data)
-		print dataDict
-
-		responseDict = self.responseDictFromInputDict(dataDict)
-		text = self.responseXML(responseDict)
-		print text
 		worker = yunCheng4Weixin.msgHandler(data)
 		self.wfile.write(worker.response())
 
-	def xmlToDict(self, xmlText):
-		xmlDict = {}
-		itemlist = ET.fromstring(xmlText)
-		for child in itemlist:
-			xmlDict[child.tag] = child.text
-		return xmlDict
-
-	def responseDictFromInputDict(self, dataDict):
-		responseDict = {}
-		responseDict['TO_USER'] = dataDict['FromUserName']
-		responseDict['FROM_USER'] = dataDict['ToUserName']
-		responseDict['TIME_STEMP'] = str(timeHelper.unixTimeStamp())
-		if dataDict['MsgType'] == 'event':
-			text = u'欢迎您关注云称客户端\n我为您提供记录和查询体重信息的服务'
-		else:
-			text = u'你好，系统尚在测试中……您刚才说的是：' + dataDict['Content']
-		responseDict['RESPONSE_CONTENT'] = text.encode('UTF-8')
-		return responseDict
-
-	def responseXML(self, dataDict):
-		text = RESPONSE_TEXT_TEMPLATE 
-		for key, value in dataDict.items():
-			parameter = '{%s}' % key
-			text = text.replace(parameter, value)
-		return text
 	
 	def verifyWeixinHeader(self):
 		self.receivedParams = self.requestGet()
